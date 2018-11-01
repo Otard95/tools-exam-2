6f8b32d0c4f4696a4da70ee0235f6ddcc3a36f95
Stian Myklebostad <stian.myklebostad@getmail.no>
2018-11-01 16:53:06 +0100

    undid merge with canvas test

--- LevelEditor/ViewModel/ViewModelLocator.cs
+++ LevelEditor/ViewModel/ViewModelLocator.cs
@@ -12,7 +12,6 @@
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Ioc;
 using Microsoft.Practices.ServiceLocation;
-using LevelEditor.Model;
 
 namespace LevelEditor.ViewModel {
     /// <summary>
