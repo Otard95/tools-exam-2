37a7492aff6c4891974dc87f6459bcb1d8b0e4ce
Michael Thomas Lee <mkt@me.com>
2018-11-03 18:44:45 +0100

    TileMap v1 done.

--- LevelEditor/Services/TileMapService.cs
+++ LevelEditor/Services/TileMapService.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,61 +16,229 @@ namespace LevelEditor.Services {
 
         private TileMapService()
         {
-               Factory = new Dictionary<string, TileMap>();          
+            Factory = new Dictionary<string, TileMap>();          
+        }
+
+        public TileMap LoadMap(string path)
+        {
+            if (Factory.TryGetValue(path, out var map))
+                return map;
+
+            var tileMap = JsonService.LoadGet<TileMap>(path);
+            Factory.Add(path, tileMap);
+            return tileMap;
         }
     }
 
     public class TileMap
     {
         public int Dimension { get; set; }
-        public HashSet<TileSet> TileSets { get; set; }
+        public List<TileSet> TileSets { get; set; }
         public Dictionary<int, int> TileSetMap { get; set; }
-        public int[][][] TilePlacements { get; set; }
+        public List<TileCoordinate> CoordinateMap { get; set; }
+        public List<TileMapping> TileMappings { get; set; }
+        public Dictionary<int, int> TilePlacements { get; set; }
 
-        public void PlaceTile(int x, int y, TileSet tileSet, int tileId) {
-            CheckTileSetExistence(tileSet);
-            TilePlacements[x][y] = new[] { tileSet.Id, TileSetMap[tileSet.Id] };
+        [JsonConstructor]
+        public TileMap(int dimension)
+        {
+            Dimension = dimension;
+            TileSets = new List<TileSet>();
+            TileSetMap = new Dictionary<int, int>();
+            CoordinateMap = new List<TileCoordinate>();
+            TileMappings = new List<TileMapping>();
+            TilePlacements = new Dictionary<int, int>();
         }
 
-        private void CheckTileSetExistence(TileSet tileSet) {
+        public void PlaceTile(int x, int y, TileSet tileSet, TileKey tileKey) {
+            CheckIfTileSetHasTileDefined(tileSet, tileKey);
+
+            var tileCoordinate = MapTileCoordinate(x, y);
+            var coordinateIndex = FindCoordinateIndex(tileCoordinate);
+
+            var tileSetMapId = FindTileSetMapping(tileSet);
+            var tileMapping = CreateTileMapping(tileSetMapId, tileKey);
+            var tileMappingIndex = FindTileMappingIndex(tileMapping);
+
+            if (TilePlacements.ContainsKey(coordinateIndex)) {
+                TilePlacements[coordinateIndex] = tileMappingIndex;
+            }
+            else {
+                TilePlacements.Add(coordinateIndex, tileMappingIndex);
+            }
+        }
+
+        public TileKey GetTileKey(TileMapping tileMapping, TileSet tileSet) {
+            var tileKey = tileSet.TileKeys.FirstOrDefault(key => key.Id == tileMapping.TileId) ??
+                          throw new TileMapLogicException($"TileKey with id: {tileMapping.TileId} undefined");
+            return tileKey;
+        }
+
+        public TileSet GetTileSetFromMapping(TileMapping tileMapping) {
+            if (!TileSetMap.ContainsKey(tileMapping.TileSetMapId))
+                throw new TileMapLogicException($"TileSetMapId not defined: {tileMapping.TileSetMapId}");
+            var tileSetId = TileSetMap[tileMapping.TileSetMapId];
+
+            var tileSet = TileSets.Count > tileSetId-1
+                ? TileSets[tileSetId-1]
+                : throw new TileMapLogicException($"TileSet with id: {tileSetId} undefined");
+            return tileSet;
+        }
+
+        private int FindTileMappingIndex(TileMapping tileMapping) {
+            if (!TileMappings.Contains(tileMapping))
+            {
+                throw new TileMapLogicException($"TileMapping not defined: {tileMapping}");
+            }
+
+            return TileMappings.IndexOf(tileMapping);
+        }
+
+        private int FindTileSetMapping(TileSet tileSet)
+        {
+            return !TileSetMap.TryGetValue(tileSet.Id, out var tileSetMapId) ? MapTileSet(tileSet) : tileSetMapId;
+        }
+
+        private static void CheckIfTileSetHasTileDefined(TileSet tileSet, TileKey tileKey) {
+            if (!tileSet.TileKeys.Contains(tileKey))
+                throw new TileMapLogicException($"TileSet does not contain a tile with id: {tileKey.Id}");
+        }
+
+        private int FindCoordinateIndex(TileCoordinate tileCoordinate) {
+            if(!CoordinateMap.Contains(tileCoordinate))
+                throw new TileMapLogicException($"TileCoordinate, {tileCoordinate}, not defined! Create a definition first.");
+            return CoordinateMap.IndexOf(tileCoordinate);
+        }
+
+        private TileCoordinate MapTileCoordinate(int x, int y) {
+            var tileCoordinate = new TileCoordinate(x, y);
+            if (CoordinateMap.Contains(tileCoordinate)) {
+                var index = CoordinateMap.IndexOf(tileCoordinate);
+                CoordinateMap[index] = tileCoordinate;
+            }
+            else {
+                CoordinateMap.Add(tileCoordinate);
+            }
+
+            return tileCoordinate;
+        }
+
+        private TileMapping CreateTileMapping(int tileSetMapId, TileKey tileKey) {
+            var tileMapping = new TileMapping {
+                TileId = tileKey.Id,
+                TileSetMapId = tileSetMapId
+            };
+            if (!TileMappings.Contains(tileMapping)) {
+                TileMappings.Add(tileMapping);
+            }
+
+            return tileMapping;
+        }
+
+        private int MapTileSet(TileSet tileSet) {
 
             if (tileSet.Dimension != Dimension)
                 throw new TileMapLogicException("TileSet dimension must match TileMap dimension");
 
-            if (!TileSets.Contains(tileSet)) {
-                TileSets.Add(tileSet);
-                var mapId = TileSetMap.Values.Max() + 1;
-                TileSetMap.Add(tileSet.Id, mapId);
-            }
+            if (TileSets.Contains(tileSet))
+                return TileSetMap[tileSet.Id];
+
+            TileSets.Add(tileSet);
+            var mapId = GetNextTileSetMapId();
+            TileSetMap.Add(tileSet.Id, mapId);
+            return TileSetMap[tileSet.Id];
         }
 
-        public TileSet GetTileSetForCoordinate(int x, int y)
-        {
-            var tileDefiniton = TilePlacements[x][y];
-            var tileMapId = tileDefiniton[0];
-            var tileSetId = TileSetMap[tileMapId];
-            return TileSets.FirstOrDefault(ts => ts.Id == tileSetId);
+        private int GetNextTileSetMapId() {
+            return TileSetMap.Count > 0 ? TileSetMap.Values.Max() + 1 : 1;
         }
 
-        public BitmapSource GetTileBitMapSourceForCoordinate(int x, int y)
-        {
-            var tileDefiniton = TilePlacements[x][y];
-            var tileId = tileDefiniton[1];
-            var tileSet = GetTileSetForCoordinate(x, y);
-            return tileSet[new TileKey {Id = tileId}];
+        public TileSet GetTileSetForCoordinate(int x, int y) {
+            var tileMapping = GetTileMapping(x, y);
+            var tileSetIdMapId = TileSetMap[tileMapping.TileSetMapId];
+            return TileSets.FirstOrDefault(ts => ts.Id == tileSetIdMapId);
         }
 
-        public int GetTileKeyForCoordinate(int x, int y) {
-            var tileDefiniton = TilePlacements[x][y];
-            return tileDefiniton[1];
+        public TileMapping GetTileMapping(int x, int y) {
+            var tileCoordinate = new TileCoordinate(x, y);
+            var coordinateIndex = FindCoordinateIndex(tileCoordinate);
+            if(!TilePlacements.TryGetValue(coordinateIndex, out var tileMappingIndex))
+                throw new TileMapLogicException($"Coordinate not placed: {tileCoordinate}");
+            return TileMappings[tileMappingIndex];
         }
 
         public void ReplaceTileSet(TileSet oldTileSet, TileSet newTileSet)
         {
             if (!TileSetMap.ContainsKey(oldTileSet.Id))
                 throw new TileMapLogicException("Cannot replace non-existing Tileset");
-            CheckTileSetExistence(newTileSet);
+            MapTileSet(newTileSet);
             TileSetMap[oldTileSet.Id] = newTileSet.Id;
         }
+
+
+    }
+
+    public class TileMapping : IEquatable<TileMapping>
+    {
+        public int TileSetMapId { get; set; }
+        public int TileId { get; set; }
+
+        public bool Equals(TileMapping other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return TileSetMapId == other.TileSetMapId && TileId == other.TileId;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((TileMapping) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return $"{TileSetMapId.GetHashCode()},{TileId.GetHashCode()}".GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+
+    public class TileCoordinate : IEquatable<TileCoordinate>
+    {
+        public TileCoordinate(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public bool Equals(TileCoordinate other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return X == other.X && Y == other.Y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((TileCoordinate) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return $"{X},{Y}".GetHashCode();
+        }
+
     }
 }
--- LevelEditor/Views/CanvasView.xaml.cs
+++ LevelEditor/Views/CanvasView.xaml.cs
@@ -1,8 +1,13 @@
-﻿using LevelEditor.ViewModel;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using LevelEditor.ViewModel;
 using System.Linq;
+using System.Management.Instrumentation;
 using System.Windows.Controls;
 using System.Windows.Input;
+using LevelEditor.Models;
 using LevelEditor.Services;
+using Newtonsoft.Json;
 using Image = System.Windows.Controls.Image;
 
 namespace LevelEditor.Views
@@ -28,30 +33,31 @@ namespace LevelEditor.Views
 
         private void GenerateTiles()
         {
+            //var map = TileMapService.Instance.LoadMap("./TileMaps/DefaultTilemap");
 
-            var tileSet = TileService.Instance.GetTileset("./TileSets/DefaultTileset");
-            var tileSource = tileSet[tileSet.TileKeys.First()];
+            var tileSetToUse = TileService.Instance.GetTileset("./TileSets/DefaultTileset");
+            var map = new TileMap(128);
+            var tileKeyToPlace = tileSetToUse.TileKeys.First();
+            map.PlaceTile(0, 0, tileSetToUse, tileKeyToPlace);
+            map.PlaceTile(1, 1, tileSetToUse, tileKeyToPlace);
+            map.PlaceTile(2, 2, tileSetToUse, tileKeyToPlace);
+            map.PlaceTile(3, 3, tileSetToUse, tileKeyToPlace);
 
-            var tile = new Image
+            foreach (var coordinate in map.CoordinateMap)
             {
-                Height = tileSet.Dimension,
-                Width = tileSet.Dimension,
-                Source = tileSource
-            };
-
-            var tile2 = new Image {
-                Height = tileSet.Dimension,
-                Width = tileSet.Dimension,
-                Source = tileSource
-            };
-
-            Canvas.SetTop(tile, 128);
-            Canvas.SetLeft(tile, 128);
-            Canvas.SetTop(tile2, 256);
-            Canvas.SetLeft(tile2, 256);
-            CanvasElement.Children.Add(tile);
-            CanvasElement.Children.Add(tile2);
-
+                var tileMapping = map.GetTileMapping(coordinate.X, coordinate.Y);
+                var tileSet = map.GetTileSetFromMapping(tileMapping);
+                var tileKey = map.GetTileKey(tileMapping, tileSet);
+                var tileSource = tileSet[tileKey];
+                var tile = new Image {
+                    Height = tileSet.Dimension,
+                    Width = tileSet.Dimension,
+                    Source = tileSource
+                };
+                Canvas.SetTop(tile, 128*coordinate.Y);
+                Canvas.SetLeft(tile, 128*coordinate.X);
+                CanvasElement.Children.Add(tile);
+            }
         }
 
     }
